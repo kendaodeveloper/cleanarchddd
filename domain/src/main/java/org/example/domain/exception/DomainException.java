ee4df048dc7881a8dd5ed1724ec4f2ce54d9b2d8
Kenneth Gottschalk de Azevedo <kendaodeveloper@gmail.com>
2023-02-19 16:43:32 -0300

    add NoStackTraceException

--- domain/src/main/java/org/example/domain/exception/DomainException.java
+++ domain/src/main/java/org/example/domain/exception/DomainException.java
@@ -4,16 +4,20 @@ import org.example.domain.validation.ValidationError;
 
 import java.util.List;
 
-public class DomainException extends RuntimeException {
+public class DomainException extends NoStackTraceException {
   private final List<ValidationError> errors;
 
-  private DomainException(final List<ValidationError> errors) {
-    super("", null, true, false);
+  private DomainException(final String message, final List<ValidationError> errors) {
+    super(message);
     this.errors = errors;
   }
 
+  public static DomainException with(final ValidationError error) {
+    return new DomainException("one or more errors occurred", List.of(error));
+  }
+
   public static DomainException with(final List<ValidationError> errors) {
-    return new DomainException(errors);
+    return new DomainException("one or more errors occurred", errors);
   }
 
   public List<ValidationError> getErrors() {
---
+++ domain/src/main/java/org/example/domain/exception/NoStackTraceException.java
@@ -0,0 +1,11 @@
+package org.example.domain.exception;
+
+public class NoStackTraceException extends RuntimeException {
+  public NoStackTraceException(final String message) {
+    this(message, null);
+  }
+
+  public NoStackTraceException(final String message, final Throwable cause) {
+    super(message, cause, true, false);
+  }
+}
