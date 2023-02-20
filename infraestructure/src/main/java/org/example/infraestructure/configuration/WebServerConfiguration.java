7c0050251e5b70e1c459c753588ed16966d2a0b6
Kenneth Gottschalk de Azevedo <kendaodeveloper@gmail.com>
2023-02-20 15:00:19 -0300

    add spring boot

---
+++ infraestructure/src/main/java/org/example/infraestructure/configuration/WebServerConfiguration.java
@@ -0,0 +1,9 @@
+package org.example.infraestructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.example")
+public class WebServerConfiguration {
+}
